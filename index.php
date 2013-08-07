d35827e1d44360c7ca01c96e44a165cbd8e13dfd
Ryan Underwood <runderworld@gmail.com>
2013-08-07 03:16:55 -0700

    oops
    forgot to add the new Vendors and Artists sections to the main template

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
 
 <?php get_template_part('section_speakers'); ?>
 
+<?php get_template_part('section_artists'); ?>
+
 <?php get_template_part('section_schedule'); ?>
 
-<?php get_template_part('section_sponsors'); ?>
+<?php get_template_part('section_vendors'); ?>
 
 <?php get_template_part('section_contact'); ?>
 
