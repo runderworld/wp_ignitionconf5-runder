cdb46da57db9b51ce7671a0c6af44cd0cf75da91
Ryan Underwood <runderworld@gmail.com>
2013-09-06 20:32:31 -0700

    yessss

--- section_registration.php
+++ section_registration.php
@@ -6,9 +6,9 @@
 			<iframe src="http://www.eventbrite.com/tickets-external?eid=7636431773&ref=etckt&v=2"
 					frameborder="0" height="214" width="98%" vspace="0" hspace="0"
 					marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+			
 			<div id="reg-childcare">
 				<h1 class="section-title"><span>Childcare</span></h1>
-				<p>Childcare is available on-site during the conference for $5 per child. <strong><u>You MUST register by September 12th</u>.</strong></p>
 				<p>Childcare is available on-site during the conference for $5 per child.</p>
 				<p><strong>IMPORTANT: <u>You MUST register by September 12th</u>!</strong></p>
 				<p style="padding-top: 20px;"><a class="btn" href="https://integration.fellowshipone.com/integration/FormBuilder/FormBuilder.aspx?fCode=NBNuH7SDjNJKXlFv/F/0Xw==&cCode=gu6bxUwJSpiBDyiUOfUi2A==" target="_blank">Register for Childcare<img class="extlink" src="<?php echo get_template_directory_uri(); ?>/images/extlink-16x16.png" title="Opens in a new window"></a></p>
