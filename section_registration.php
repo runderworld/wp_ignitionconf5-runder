c0b8773d0e7339c79cf8bf191499995ec3a59f51
Ryan Underwood <runderworld@gmail.com>
2013-09-15 09:56:59 -0700

    change childcare deadline

--- section_registration.php
+++ section_registration.php
@@ -14,7 +14,7 @@
 			<div id="reg-childcare">
 				<h1 class="section-title"><span>Childcare</span></h1>
 				<p>Childcare is available on-site during the conference for $5 per child.</p>
-				<p><strong>IMPORTANT: <u>You MUST register by September 12th</u>!</strong></p>
+				<p><strong>IMPORTANT: <u>You MUST register by Monday, September 16th</u>!</strong></p>
 				<p style="padding-top: 20px;"><a class="btn" href="https://integration.fellowshipone.com/integration/FormBuilder/FormBuilder.aspx?fCode=NBNuH7SDjNJKXlFv/F/0Xw==&cCode=gu6bxUwJSpiBDyiUOfUi2A==" target="_blank">Register for Childcare<img class="extlink" src="<?php echo get_template_directory_uri(); ?>/images/extlink-16x16.png" title="Opens in a new window"></a></p>
 			</div>
 		</div> <!-- .row < #registration -->
