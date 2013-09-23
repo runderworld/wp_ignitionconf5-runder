9033fbe488cc1d98497ed3add96cea46dcf4bcab
Ryan Underwood <runderworld@gmail.com>
2013-09-23 12:22:53 -0700

    close early 2014 registration
    keep open registration info commented out (though not served to client)

--- section_registration.php
+++ section_registration.php
@@ -3,14 +3,22 @@
 		<div class="row">
 			<div id="reg-register">
 				<h1 class="section-title"><span>Registration</span></h1>
+				<p>Thank you for attending ANTHEM Conference 2013!</p>
+				<p>We are already hard at work planning for 2014 &mdash; information is forthcoming.</p>
+				<?php
+				/*
 				<p>FRIDAY: Check-in and registration opens at 6pm in the Rock Park. Event is from 7pm to 9pm.</p>
 				<p>SATURDAY: Check-in and registration opens at 7:30am in the Rock Park. Event is from 9am to 5pm.</p>
+				*/
+				?>
 			</div>
-			
+			<?php
+				/*
 			<iframe src="http://www.eventbrite.com/tickets-external?eid=8438797671&ref=etckt&v=2"
 					frameborder="0" height="214" width="98%" vspace="0" hspace="0"
 					marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
-			
+			*/
+			?>
 			<div id="reg-childcare">
 				<h1 class="section-title"><span>Childcare</span></h1>
 				<p>Childcare is available on-site during the conference for $5 per child.</p>
